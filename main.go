e7920c08bdb364769f50db1622ee817ed8ad9c85
Weather <weathergod3218@gmail.com>
2026-05-02 02:23:16 -0400

    feat: Webpage working

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	//cshAuth "github.com/computersciencehouse/csh-auth"
+	"github.com/gin-gonic/gin"
+)
+
+var SERVER_HOST = os.Getenv("SERVER_HOST")
+
+func main() {
+	router := gin.Default()
+
+	// csh := cshAuth.CSHAuth{}
+
+	// csh.Init(
+	// 	os.Getenv("AUTH_OIDC_ID"),
+	// 	os.Getenv("AUTH_OIDC_SECRET"),
+	// 	os.Getenv("AUTH_JWC_SECRET"),
+	// 	os.Getenv("VOTE_STATE"),
+	// 	os.Getenv("SERVER_HOST"),
+	// 	os.Getenv("SERVER_HOST")+"/auth/callback",
+	// 	os.Getenv("SERVER_HOST")+"/auth/login",
+	// 	[]string{"profile", "email", "groups"},
+	// )
+
+	// router.GET("/auth/login", csh.AuthRequest)
+	// router.GET("/auth/callback", csh.AuthCallback)
+	// router.GET("/auth/logout", csh.AuthLogout)
+
+	router.StaticFS("/static", http.Dir("static"))
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "My Site",
+		})
+	})
+
+	router.Run(":8080")
+}
