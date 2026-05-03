32e29eef4597af24878af2430c33accb2f3525d2
Weather <weathergod3218@gmail.com>
2026-05-03 00:50:54 -0400

    feat: Webhooks to OKD

--- api.go
+++ api.go
@@ -6,11 +6,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func GetHomepage(c *gin.Context) {
+func GetHomePage(c *gin.Context) {
 	// This is intentionally left unprotected
 	// A user may be unable to vote but should still be able to see a list of polls
 
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"Counters": "Hello",
+	})
+}
+
+func GetCreate(c *gin.Context) {
+	c.HTML(http.StatusOK, "create.tmpl", gin.H{
+		"No": "Yes",
 	})
 }
